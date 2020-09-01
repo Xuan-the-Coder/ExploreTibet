b3c8e2584feb440eda8a226fa95931f0f4cc7649
Xuan_the_Coder <wx209dewang@gmail.com>
2020-08-31 23:43:40 -0500

    created home and blog controllers

---
+++ ExploreTibet/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreTibet.Controllers
+{
+    public class HomeController
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = " Hello, ASP.NET Core MVC" };
+        }
+    }
+}
