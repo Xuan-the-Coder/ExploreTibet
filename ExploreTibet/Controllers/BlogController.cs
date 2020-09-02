e9069930e19bb8163b5ba597c23acd3fabfe4639
Xuan_the_Coder <wx209dewang@gmail.com>
2020-09-01 23:11:13 -0500

    added customized url

--- ExploreTibet/Controllers/BlogController.cs
+++ ExploreTibet/Controllers/BlogController.cs
@@ -6,11 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreTibet.Controllers
 {
+    [Route("blog")]
     public class BlogController : Controller
     {
         public IActionResult Index()
         {
             return new ContentResult { Content = "Blog posts." };
+        }
+
+        [Route("{year:int}/{month:int}/{key}")]
+        public IActionResult Post(int year, int month, int key)
+        {
+            return new ContentResult { Content = string.Format("Year: {0}; Month:{1}' Key: {2}", year, month, key) };
         }
     }
 }
